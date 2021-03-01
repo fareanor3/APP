2f3803b8ccd8e82ce59492445f106af12af638fa
fareanor3 <61111238+fareanor3@users.noreply.github.com>
2021-03-01 09:18:44 +0100

    Create bataille_a_deux_26tours.py

--- APP1/bataille_a_deux_26tours.py
+++ APP1/bataille_a_deux_26tours.py
@@ -36,7 +36,8 @@ random.shuffle(joueur2)
 cj1 = []
 cj2 = []
 n=0
-
+compte1=0
+compte2=0
 
 while n<26:
     
@@ -66,6 +67,7 @@ while n<26:
         joueur1.extend(cj2)
         cj1 = []
         cj2 = []
+        compte1 += 1
         
     elif score_a1<score_a2: # joueur 2 gagne
 
@@ -73,13 +75,17 @@ while n<26:
         joueur2.extend(cj1)
         cj1 = []
         cj2 = []
-        
+        compte2 +=1
         
     print("joueur 1 ", len(joueur1))
     print("joueur 2 ", len(joueur2))
 
 if len(joueur1) > len(joueur2) : 
-    print("Le joueur 1 a gagné")    
+    print("le joueur 1 a gagné")
+    print(compte1)
+    print("à : ",compte2)
 
 elif len(joueur1) < len(joueur2) :
-    print("Le joueur 2 a Gagné")
+    print("le joueur 2 a gagné")
+    print(compte2)
+    print("à : ",compte1)
