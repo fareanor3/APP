77d6f660ecd9cf94a978a2fc8afd8e2be70c28d6
fareanor3 <61111238+fareanor3@users.noreply.github.com>
2021-03-01 09:06:14 +0100

    Update bataille_decouverte_26tours.py

--- APP1/bataille_decouverte_26tours.py
+++ APP1/bataille_decouverte_26tours.py
@@ -1,10 +1,3 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Sat Feb 27 12:23:45 2021
-
-@author: loris
-"""
-
 import random
 
 def jeu_cartes():
@@ -44,79 +37,31 @@ n=0
 
 while n<26:
 
-    cj1.append(joueur1[0])
-    cj2.append(joueur2[0])
+    cj1.append(joueur1.pop(0))
+    cj2.append(joueur2.pop(0))
     n+=1
     print(n)
 
     if cj1[-1][1]>cj2[-1][1]: #joueur 1 gagne
 
-        joueur2.pop(0)   # ajoute l'elment de la liste à la fin
-        joueur1.pop(0)
-        joueur1.append(cj2)
-        joueur1.append(cj1)
+        joueur1.extend(cj1)   # ajoute l'elment de la liste à la fin
+        joueur1.extend(cj2)
+        cj1=[]
+        cj2=[]
+
         
         print("joueur 1 gagne")
 
     elif cj1[-1][1]<cj2[-1][1]: # joueur 2 gagne
 
-        joueur1.pop(0)   # ajoute l'elment de la liste à la fin
-        joueur2.pop(0)
-        joueur2.append(cj1)
-        joueur2.append(cj2)
+        joueur2.extend(cj1)   # ajoute l'elment de la liste à la fin
+        joueur2.extend(cj2)
+        cj1=[]
+        cj2=[]
         
         print("joueur 2 gagne")
         
-    elif cj1[-1][1]==cj2[-1][1]:
-        print("bataille")
-        
-        cj1.append(joueur1[1])
-        cj2.append(joueur2[1])
-
-        joueur1.pop(0)
-        joueur1.pop(0)
-        joueur2.pop(0)
-        joueur2.pop(0)
-
-        if cj1[-1][1]>cj2[-1][1]: #joueur 1 gagne
-
-            joueur1.append(cj2[-2])
-            joueur1.append(cj2[-1])
-            joueur1.append(cj1[-2])
-            joueur1.append(cj1[-1])
-        
-        elif cj1[-1][1]<cj2[-1][1]: # joueur 2 gagne
-
-            joueur2.append(cj1[-2])
-            joueur2.append(cj1[-1])
-            joueur2.append(cj2[-2])
-            joueur2.append(cj2[-1])
-        
-        while cj1[-1][1]==cj2[-1][1]:
-            cj1.append(joueur1[0])
-            cj2.append(joueur2[0])
-            joueur1.pop(0)
-            joueur2.pop(0)
-
-            
-            if cj1[-1][1]>cj2[-1][1]: #joueur 1 gagne
-
-                joueur1.append(cj2[-2])
-                joueur1.append(cj2[-1])
-                joueur1.append(cj1[-2])
-                joueur1.append(cj1[-1])
-        
-            elif cj1[-1][1]<cj2[-1][1]: # joueur 2 gagne
-
-                joueur2.append(cj1[-2])
-                joueur2.append(cj1[-1])
-                joueur2.append(cj2[-2])
-                joueur2.append(cj2[-1])
-
-    print("joueur 1 ", len(joueur1))
-    print("joueur 2 ", len(joueur2))
-
-
+    
 if len(joueur1) > len(joueur2) : 
     print("le joueur 1 a Gagner")
 
