2640ac9df005ad70a912f3bb3a21db390beb0eab
fareanor3 <61111238+fareanor3@users.noreply.github.com>
2021-03-15 16:12:22 +0100

    modif code

--- APP2/Harversine.py
+++ APP2/Harversine.py
@@ -1,22 +1,26 @@
-
 import math
 
-home = [49.83,3.15]
-work = [45.99,4.78]
+home = [49.83,56.89]
+work = [37.56,61.41]
 
 def distance(origin, destination):
     lat1, lon1 = origin
     lat2, lon2 = destination
-    radius = 6371 # km
+    radius = 6378.140 # km
 
     dlat = math.radians(lat2-lat1)
     dlon = math.radians(lon2-lon1)
-    a = math.sin(dlat/2) * math.sin(dlat/2) + math.cos(math.radians(lat1)) \
+    a = math.sin(dlat/2) **2 + math.cos(math.radians(lat1)) \
         * math.cos(math.radians(lat2)) * math.sin(dlon/2) * math.sin(dlon/2)
     c = 2 * math.atan2(math.sqrt(a), math.sqrt(1-a))
     d = radius * c
 
     return d
 
-d1 = distance(home,work)
-print(d1, "km")
+d = distance(home,work)
+print(d,"km")
+
+file=open("villes.csv","r")
+test=csv.reader(file)
+for row in test:
+    print(row[1])
