0a4bf7eaa8bfe996f0b8645eff689815f5fc68f6
fareanor3 <61111238+fareanor3@users.noreply.github.com>
2021-03-28 20:33:16 +0200

    nothing

--- APP2/Harversine.py
+++ APP2/Harversine.py
@@ -28,5 +28,9 @@ for line in lines:
 for gps in all_gps: # ne change pas la complexité car boucle 1 + 2 car les deux s'execute pour ttes les lignes
     coords = gps[1].split(",")
     print(gps[0],distance(home,[float(coords[0]),float(coords[1])]))
+
+    #num_list = [1,2,3,4]
+    #name_list = ["one","two","three","four"]
+    #print ({name_list[i]:num_list[i] for i in range(len(num_list))
+
     #villes(zip
-print(villes)
