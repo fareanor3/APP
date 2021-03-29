4ced0961602473abdcc95cababef679880388fd5
fareanor3 <61111238+fareanor3@users.noreply.github.com>
2021-03-29 10:29:44 +0200

    update algo

--- APP2/Harversine.py
+++ APP2/Harversine.py
@@ -28,9 +28,39 @@ for line in lines:
 for gps in all_gps: # ne change pas la complexité car boucle 1 + 2 car les deux s'execute pour ttes les lignes
     coords = gps[1].split(",")
     print(gps[0],distance(home,[float(coords[0]),float(coords[1])])) # passer sous la forme d'un dico ( tuple )
-
+    d = {k:v for k, v in iterable}
     #num_list = [1,2,3,4]
     #name_list = ["one","two","three","four"]
     #print ({name_list[i]:num_list[i] for i in range(len(num_list))
 
     #villes(zip
+
+def fusion(liste1,liste2):
+    liste=[]
+    i,j=0,0
+    while i<len(liste1)and j<len(liste2):
+        if liste1[i]<=liste2[j]:
+            liste.append(liste1[i])
+            i+=1
+        else:
+            liste.append(liste2[j])
+            j+=1
+    while i<len(liste1):
+        liste.append(liste1[i])
+        i+=1
+    while j<len(liste2):
+        liste.append(liste2[j])
+        j+=1
+    return liste
+
+def tri_fusion(liste):
+    if len(liste)<2:
+        return liste[:]
+    else:
+        milieu=len(liste)//2
+        liste1=tri_fusion(liste[:milieu])
+        liste2=tri_fusion(liste[milieu:])
+        return fusion(liste1,liste2)
+
+L=[3,4,6,2,5,1,8,7]
+print(tri_fusion(L))
